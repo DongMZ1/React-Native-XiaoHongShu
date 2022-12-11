ea5e24542642d82984d355f8b125663739e03dff
DongMZ1 <915094594@qq.com>
2022-12-10 16:41:57 -0800

    stuiped native navigation gives me a default bg

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
     const Routes = createNativeStackNavigator();
     return (
-        <NavigationContainer><Routes.Navigator initialRouteName='login' >
+        <NavigationContainer theme={{...DefaultTheme, colors: {...DefaultTheme.colors, background: 'white'}}}><Routes.Navigator initialRouteName='login' >
             <Routes.Screen name='login' options={{ headerShown: false }} >{(props) => <View>login page</View>}</Routes.Screen>
             <Routes.Screen name='home' options={{ headerShown: false }} >{(props) => <View>home page</View>}</Routes.Screen>
         </Routes.Navigator></NavigationContainer>
