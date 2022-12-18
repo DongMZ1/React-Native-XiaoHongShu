f9a6e4dde7bcda433f77a28fb0eaea8123201d25
DongMZ1 <915094594@qq.com>
2022-12-18 15:34:15 -0800

    add an animated ui component for share

--- src/view/login/Login.tsx
+++ src/view/login/Login.tsx
@@ -2,10 +2,14 @@ import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AppRoutesType } from '../../types/routes/appRoutes';
 import { View, Button, Text } from 'react-native'
+import { AnimatedView } from '../../shared/components/AnimatedView';
+import { useState } from 'react';
 type LoginScreenRouteProp = NativeStackScreenProps<AppRoutesType, 'Login'>;
 export const Login = ({ route, navigation }: LoginScreenRouteProp) => {
     const navigator = useNavigation()
+    const [show, setshow] = useState(false);
     return <View>
-        <Button onPress={e => navigator.navigate('Home', { screen: 'Discover' })} title="Login" />
+        <AnimatedView show={show} time={200} animationType={['Opacity']}><Text>This thing should fade in and fade out</Text></AnimatedView>
+        <Button onPress={() => setshow(state => !state)} title="Switch" />
     </View>
 }
